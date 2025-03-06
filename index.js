47e092dd147ea3fe0043294d58ab81c58c55032e
rivki <rivki2676@gmail.com>
2025-03-06 22:41:01 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import renderApi from '@api/render-api';
 
-renderApi.auth('rnd_wra223DHBda9dyyheGWZCJV28Tb3');
+renderApi.auth('rnd_TcqV0fuUES1YXBEfEk7dTsC49haO');
 renderApi.listServices({includePreviews: 'true', limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
