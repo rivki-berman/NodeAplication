2daf0cf191312bec6cc1d7c5261e7c6f08428de2
rivki <rivki2676@gmail.com>
2025-03-09 16:37:08 +0200

    get_function

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import renderApi from '@api/render-api';
+import express from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
+import bodyParser from "body-parser";
 
-renderApi.auth('rnd_TcqV0fuUES1YXBEfEk7dTsC49haO');
-renderApi.listServices({includePreviews: 'true', limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+const app = express()
+const port = 3001
+dotenv.config()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('', (req, res) => {
+  renderApi.auth(process.env.RENDER_API_KEY);
+  renderApi.listServices({ includePreviews: 'true', limit: '20' })
+    .then( data  => {return res.status(200).send(data)})
+    .catch(err => console.error(err));
+})
+
+app.listen(port, () => {
+  console.log(`my application is listening on http://localhost:${port}`);
+})
